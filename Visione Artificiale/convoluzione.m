db4b2d2281d45db6b7d5b2945baa08876cba4a23
Federico Agnello <78120567+FedeDC512@users.noreply.github.com>
2025-04-28 17:38:38 +0200

    Upload exercises

---
+++ Visione Artificiale/convoluzione.m
@@ -0,0 +1,35 @@
+function output = convoluzione(img, k)
+    % Converte l'immagine in single precision per il calcolo
+    img = single(img);
+
+    % Ottieni le dimensioni dell'immagine e del kernel
+    [h, w] = size(img);
+    [kh, kw] = size(k);
+
+    % Calcola gli offset per il centro del kernel
+    a = floor(kh / 2);
+    b = floor(kw / 2);
+
+    % Inizializza la nuova immagine con zeri (stessa dimensione dell'originale)
+    temp = zeros(h, w, 'single');
+
+    % Scansiona l'immagine ignorando i bordi
+    for j = 1 + a : h - a
+        for i = 1 + b : w - b
+            temp = 0; % Accumulatore del valore convoluto
+
+            % Applica il kernel con doppio for
+            for x = -a : a
+                for y = -b : b
+                    temp = temp + img(j - y, i - x) * k(y + a + 1, x + b + 1);
+                end
+            end
+
+            % Assegna il valore convoluto alla nuova immagine
+            output(j, i) = temp;
+        end
+    end
+
+    % Converte il risultato finale in uint8 per la visualizzazione
+    output = uint8(output);
+end
---
+++ Visione Artificiale/convoluzione_con_padding.m
@@ -0,0 +1,28 @@
+function output = convoluzione_con_padding(img, k, padding_type)
+    [h, w] = size(img);    % Dimensioni immagine originale
+    [kh, kw] = size(k);    % Dimensioni del kernel
+    
+    a = floor(kh / 2);     % Offset verticale
+    b = floor(kw / 2);     % Offset orizzontale
+    
+    % Aggiungi il padding specificato (replicate, symmetric, circular, ecc.)
+    padded_img = padarray(img, [a, b], padding_type);
+    
+    output = zeros(h, w, 'single');  % Matrice per l'output
+
+    % Loop sulla versione originale dell'immagine
+    for j = 1:h
+        for i = 1:w
+            temp = 0;
+            for x = -a:a
+                for y = -b:b
+                    % Accedi alla versione paddata con un offset
+                    temp = temp + padded_img(j + a - y, i + b - x) * k(y + a + 1, x + b + 1);
+                end
+            end
+            output(j, i) = temp;
+        end
+    end
+    
+    output = uint8(output); % Conversione a immagine
+end
